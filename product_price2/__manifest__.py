23de3d831072ca9b26b2571ffa5bb08dce1e6075
unknown <euclidesollarves@gmail.com>
2023-08-04 11:46:04 -0400

    creacion de addons precio 2

--- product_price2/__manifest__.py
+++ product_price2/__manifest__.py
@@ -1,35 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "product_price2",
+    'name': "Product Price 2",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Agregar un nuevo campo para precio 2 en el formulario del producto""",
 
     'description': """
-        Long description of module's purpose
+        Agregar un nuevo campo para precio 2 en el formulario del producto
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Euclides Ollarves",
+    'website': "https://www.instagram.com/Euclides_Ollarves",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sale',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['product'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/product_view.xml'
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 }
